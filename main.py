98b682b9490ebce7f3103d102eb167824c662d92
Adrian Kodjasz <akodjasz@ypsilon.net>
2020-09-27 20:43:30 +0200

    main.py - added user selection for visit searching

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 import requests
-from tools import saveFile, _printer
+from tools import saveFile, _printer, getToday, getDeltaDate
 import config
 
 
@@ -27,9 +27,11 @@ class Luxmed:
         self._URL_MAIN_PAGE = self._URL + 'Account/LogOn'
         self._URL_LOGIN = self._URL + 'Account/LogIn'
         self._URL_GROUPS = self._URL + '/NewPortal/Dictionary/serviceVariantsGroups'
-        self._URL_SEARCH = self._URL + '/NewPortal/terms/index?cityId=5&serviceVariantId=13410&languageId=10&searchDateFrom=2020-09-17&searchDateTo=2020-09-30&searchDatePreset=14&=,&processId=a2ed410b-17c9-4338-a801-6a1a9ce3f608&nextSearch=false&searchByMedicalSpecialist=false&isSecondServiceVariantPostTriage=false'
+        self._URL_SEARCH = self._URL + '/NewPortal/terms/index'
         # CONFIG
         self._DEBUG = True
+
+        self.service_groups = None
 
     def getMainPage(self, s):
         """Initiate main page to get all cookies
@@ -75,19 +77,67 @@ class Luxmed:
             'X-Is-RWD': 'false',
         })
         response = session.get(self._URL_GROUPS, headers=self.headers)
-        print(response.status_code)
         if self._DEBUG:
             saveFile('groups_page', response.text)
-        _printer(header='RESPONSE', data=response.content)
+        # _printer(header='RESPONSE', data=response.content)
+        self.service_groups = response.json()
         return response.json()
 
     def searchVisits(self):
         session = self.session
-        response = session.get(self._URL_SEARCH)
+
+        varieties = {}
+        for index, exam in enumerate(self.service_groups, start=1):
+            name = exam['name']
+            print(f'{index}. {name}')
+            variety = {
+                'name': exam['name'],
+                'id': exam['id']
+            }
+
+            exams_dict = {}
+            for i, subexam in enumerate(exam['children'], start=1):
+                exams_dict.update({
+                    i: {
+                        'name': subexam['name'],
+                        'id': subexam['id'],
+                    }
+                })
+            variety['examList'] = exams_dict
+            varieties.update({
+                index: variety,
+            })
+
+        user_choice_exam = int(
+            input(f'Wybierz usługę z listy powyżej. [{min(varieties.keys())}-{max(varieties.keys())}]'))
+
+        if len(varieties[user_choice_exam]['examList']) > 0:
+            for i, exam in varieties[user_choice_exam]['examList'].items():
+                name = exam['name']
+                print(f'{i}. {name}')
+
+        user_choice_subexam = int(input(f'Wybierz konkretne badanie z listy powyżej. '
+                                        f'[{min(varieties.keys())}-{max(varieties.keys())}]'))
+
+        postData = {
+            'serviceVariantId': varieties[user_choice_exam]['examList'][user_choice_subexam]['id'],
+            'cityId': '5',
+            'languageId': '10',
+            'searchDateFrom': getToday(),
+            # for now search default 2 weeks
+            'searchDateTo': getDeltaDate(days=14),
+            'searchDatePreset': '14',
+            'processId': '0c7f68f8-d62b-4db4-a949-7cbc6599ea0b',
+            # 'secondServiceVariantId': '12574',
+            'nextSearch': 'false',
+            'searchByMedicalSpecialist': 'false',
+            'isSecondServiceVariantPostTriage': 'true',
+            '': '',
+        }
+
+        response = session.get(self._URL_SEARCH, params=postData)
         if self._DEBUG:
             saveFile('visits', response.text)
-        print(response.status_code)
-        pprint(response.json())
 
 
 if __name__ == '__main__':
--- tools.py
+++ tools.py
@@ -1,9 +1,11 @@
 import json
 from pprint import pprint
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 
 def saveFile(filename, data):
-    with open(f'{filename}.html', 'w') as f:
+    with open(f'{filename}.json', 'w') as f:
         f.write(data)
         # print(f'File {filename} successfully saved!')
     return
@@ -19,3 +21,20 @@ def _printer(header, data):
     if data:
         pprint(data)
     print('[{} {} {}]'.format(25 * '#', header, 25 * '#'))
+
+
+def getToday():
+    today = date.today().strftime('%Y-%m-%d')
+    return today
+
+
+def getDeltaDate(**kwargs):
+    if kwargs.get('years'):
+        pass
+    if kwargs.get('months'):
+        pass
+    if kwargs.get('days'):
+        print(date.today())
+        newDate = date.today() + relativedelta(days=+kwargs['days'])
+
+    return newDate.strftime('%Y-%m-%d')
