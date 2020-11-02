8e7952d84cd9ce73b1827b6f6b364b00cc55ae08
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2020-11-03 00:59:00 +0100

    flaskServer.py - added wrong login exception, parse visits after success login and return to user

--- flaskServer.py
+++ flaskServer.py
@@ -3,33 +3,38 @@ from pprint import pprint
 from flask import Flask, redirect, url_for, request, render_template
 from main import Luxmed
 import config
-
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # luxmed = Luxmed(config.email, config.password)
         email = request.form['login']
         password = request.form['password']
-        luxmed = Luxmed(email, password)
-        return redirect(url_for('search', luxmed=luxmed))
 
-    return render_template('login.html')
+        return redirect(url_for('search', login=email, password=password))
+
+    message = request.args.get('message', '')
+    return render_template('login.html', message=message)
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'GET':
         pprint(request)
-        luxmed = request.args['luxmed']
-        print(luxmed)
-        # luxmed = Luxmed(config.email, config.password)
-        # luxmed.getLogin()
-        # luxmed.getGroupsIds()
-        # varieties = luxmed.parseVarieties()
-        from varieties import varieties
+        email = request.args['login']
+        password = request.args['password']
+        luxmed = Luxmed(login=email, password=password)
+        content = {}
+
+        luxmed.getMainPage()
+        try:
+            session = luxmed.getLogin()
+        except Exception as e:
+            return redirect(url_for('login', message=str(e)))
+
+        luxmed.getGroupsIds()
+        varieties = luxmed.parseVarieties()
+        # from varieties import varieties
         return render_template('index.html', varieties=varieties)
     else:
         pass
