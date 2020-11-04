150c498b5b0c276810e218507a40db1610346f66
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2020-11-04 01:03:15 +0100

    flaskServer.py - start luxmed logging process login form is send

--- flaskServer.py
+++ flaskServer.py
@@ -3,15 +3,28 @@ from pprint import pprint
 from flask import Flask, redirect, url_for, request, render_template
 from main import Luxmed
 import config
+
 app = Flask(__name__)
+
+storage = {}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['login']
         password = request.form['password']
+        luxmed = Luxmed(login=email, password=password)
+        storage['luxmed'] = luxmed
 
-        return redirect(url_for('search', login=email, password=password))
+        luxmed.getMainPage()
+
+        try:
+            username = luxmed.getLogin()
+        except Exception as e:
+            return redirect(url_for('login', message=str(e)))
+
+        return redirect(url_for('search', username=username[0]))
 
     message = request.args.get('message', '')
     return render_template('login.html', message=message)
@@ -20,21 +33,11 @@ def login():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'GET':
-        pprint(request)
-        email = request.args['login']
-        password = request.args['password']
-        luxmed = Luxmed(login=email, password=password)
         content = {}
-
-        luxmed.getMainPage()
-        try:
-            session = luxmed.getLogin()
-        except Exception as e:
-            return redirect(url_for('login', message=str(e)))
-
+        username = request.args.get('username')
+        luxmed = storage['luxmed']
         luxmed.getGroupsIds()
         varieties = luxmed.parseVarieties()
-        # from varieties import varieties
-        return render_template('index.html', varieties=varieties)
+        return render_template('index.html', varieties=varieties, username=username)
     else:
         pass
